03cef11c916cb04952fe945d24f24bbaba8fc721
Lucas Ivisson <53708095+lucasivisson@users.noreply.github.com>
2023-12-10 18:04:39 -0300

    Create data-udp.py

---
+++ data-udp.py
@@ -0,0 +1,33 @@
+Server listening on UDP port 5001
+UDP buffer size: 208 KByte (default)
+
+[1] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 56232
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[1] 0.0000-10.0114 sec 1.25 MBytes 1.05 Mbits/sec 0.009 ms 0/895 (0%)
+[2] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 43213
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[2] 0.0000-10.0155 sec 1.25 MBytes 1.05 Mbits/sec 0.004 ms 0/895 (0%)
+[3] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 40923
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[3] 0.0000-10.0158 sec 1.25 MBytes 1.05 Mbits/sec 0.003 ms 0/895 (0%)
+[4] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 44352
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[4] 0.0000-10.0155 sec 1.25 MBytes 1.05 Mbits/sec 0.006 ms 0/895 (0%)
+[5] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 48064
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[5] 0.0000-10.0157 sec 1.25 MBytes 1.05 Mbits/sec 0.005 ms 0/895 (0%)
+[6] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 50357
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[6] 0.0000-10.0155 sec 1.25 MBytes 1.05 Mbits/sec 0.003 ms 0/895 (0%)
+[7] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 54662
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[7] 0.0000-10.0156 sec 1.25 MBytes 1.05 Mbits/sec 0.004 ms 0/895 (0%)
+[8] local 10.0,0,0,2 port 5001 connected with 10.0.0.10 port 50183
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[8] 0.0000-10.0156 sec 1.25 MBytes 1.05 Mbits/sec 0.006 ms 0/895 (0%)
+[9] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 56482
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[9] 0.0000-10.0156 sec 1.25 MBytes 1.05 Mbits/sec 0.006 ms 0/895 (0%)
+[10] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 50308
+Interval | Transfer | Bandwidth | Jitter | Lost/Total Datagrams
+[10] 0.0000-10.0153 sec 1.25 MBytes 1.05 Mbits/sec 0.008 ms 0/895 (0%)
