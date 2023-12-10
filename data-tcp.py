5ca5848cdb21ac64b3957c641d53f36fb37537a6
Lucas Ivisson <53708095+lucasivisson@users.noreply.github.com>
2023-12-10 18:04:52 -0300

    Create data-tcp.py

---
+++ data-tcp.py
@@ -0,0 +1,33 @@
+Server listening on TCP port 5001
+TCP window size: 85.3 KByte (default)
+
+[1] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 42002
+[ID] | Interval | Transfer | Bandwidth
+[1] 0.0000-10.0001 sec 38.4 GBytes 32.9 Gbits/sec
+[2] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 51952
+[ID] | Interval | Transfer | Bandwidth
+[2] 0.0000-10.0003 sec 38.4 GBytes 33.0 Gbits/sec
+[3] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 45610
+[ID] | Interval | Transfer | Bandwidth
+[3] 0.0000-10.0004 sec 38.6 GBytes 33.2 Gbits/sec
+[4] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 34764
+[ID] | Interval | Transfer | Bandwidth
+[4] 0.0000-10.0005 sec 37.5 GBytes 32.2 Gbits/sec
+[5] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 45744
+[ID] | Interval | Transfer | Bandwidth
+[5] 0.0000-10.0005 sec 36.9 GBytes 31.7 Gbits/sec
+[6] local 10.0.0.2 port 5001 connected with 10,0,0,10 port 34578
+[ID] | Interval | Transfer | Bandwidth
+[6] 0.0000-10.0004 sec 36.6 GBytes 31.4 Gbits/sec
+[7] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 60098
+[ID] | Interval | Transfer | Bandwidth
+[7] 0.0000-10.0002 sec 36.6 GBytes 31.5 Gbits/sec
+[8] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 37708
+[ID] | Interval | Transfer | Bandwidth
+[8] 0.0000-10.0004 sec 39.2 GBytes 33.7 Gbits/sec
+[9] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 54972
+[ID] | Interval | Transfer | Bandwidth
+[9] 0.0000-10.0005 sec 37.6 GBytes 32.3 Gbits/sec
+[10] local 10.0.0.2 port 5001 connected with 10.0.0.10 port 42264
+[ID] | Interval | Transfer | Bandwidth
+[10] 0.0000-10.0004 sec 37.8 GBytes 32.4 Gbits/sec
